327f0fdd7b2b8c0f0abe6c3bad814d5bbb1d49a7
Hoa Tran <hoa.thanh.tran@dfo.global>
2021-09-25 16:15:53 +0700

    Init App

---
+++ src/components/InboxScreen.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Proptypes from "prop-types";
+import {connect} from "react-redux";
+import TaskList from "./TaskList";
+
+export function PureInboxScreen({ error }) {
+  if (error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <div className="icon-face-sad"/>
+          <div className="title-message">Oh no!</div>
+          <div className="subtitle-message">Something went wrong</div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">
+          <div className="title-wrapper">Task Box</div>
+        </h1>
+      </nav>
+      <TaskList/>
+    </div>
+  )
+}
+
+PureInboxScreen.propTypes = {
+  error: Proptypes.string
+}
+
+PureInboxScreen.defaultProps = {
+  error: null
+}
+
+export default connect(({ error }) => ({ error }))(PureInboxScreen)
---
+++ src/components/constants.js
@@ -0,0 +1,3 @@
+export const TASK_INBOX = 'TASK_INBOX'
+export const TASK_PINNED = 'TASK_PINNED'
+export const TASK_ARCHIVED = 'TASK_ARCHIVED'
